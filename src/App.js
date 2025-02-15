1757abd22dc50d7a2e1fc55ee4153980f07022e6
chrisShort32 <144724027+chrisShort32@users.noreply.github.com>
2025-02-15 19:04:06 +0000

    Added tos and traits to matches

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Login from './components/Login';
 import Home from './components/Home';
 import TermsOfService from "./components/TermsOfService";
+import Profile from './components/Profile';
+import Messages from './components/Messages';
+import Matches from './components/Matches'; // <-- Import the new Matches component
 
 function App() {
   return (
@@ -12,8 +15,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/tos" element={<TermsOfService />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/matches" element={<Matches />} />  {/* <-- Matches route */}
           <Route path="/" element={<Navigate replace to="/login" />} />
-        
         </Routes>
       </div>
     </Router>
